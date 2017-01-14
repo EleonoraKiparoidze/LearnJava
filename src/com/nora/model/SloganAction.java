8627b2c842475d2b7c93d544095d960413652111
EleonoraNora <kiparoidzie@gmil.com>
2017-01-14 09:22:59 +0200

    Add SloganAction model and Main class to run program

---
+++ src/com/nora/model/SloganAction.java
@@ -0,0 +1,10 @@
+package com.nora.model;
+
+/**
+ * Created by nora on 14.01.17.
+ */
+public class SloganAction {
+    public void printSlogan(){
+        System.out.println("Learn Java!");
+    }
+}
---
+++ src/com/nora/run/Main.java
@@ -0,0 +1,11 @@
+package com.nora.run;
+import com.nora.model.SloganAction;
+/**
+ * Created by nora on 14.01.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        SloganAction sloganObject = new SloganAction();
+        sloganObject.printSlogan();
+    }
+}
