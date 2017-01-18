1c62152495c99cab81734c85f9765b19859624ba
EleonoraNora <kiparoidzie@gmil.com>
2017-01-18 18:16:36 +0200

    Add function to sort ingredients by calories

--- src/com/nora/chapter4/logic/SaladCalculator.java
+++ src/com/nora/chapter4/logic/SaladCalculator.java
@@ -3,12 +3,15 @@ package com.nora.chapter4.logic;
 import com.nora.chapter4.controller.Salad;
 import com.nora.chapter4.controller.Vegetable;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * Created by nora on 18.01.17.
  */
-public class SaladCalculator {
+public class SaladCalculator implements Serializable {
 
     public static double CalculateSaladCalories(Salad salad){
        ArrayList<Vegetable> ingredients = salad.getIngredients();
@@ -17,5 +20,23 @@ public class SaladCalculator {
             totalCalories += VegetableCalculator.getTotalCalories(vegetable);
         }
         return totalCalories;
+    }
+
+    public static double CalculateSaladWeight(Salad salad){
+        ArrayList<Vegetable> ingredients = salad.getIngredients();
+        double totalWeight = 0;
+        for(Vegetable vegetable:ingredients){
+            totalWeight += vegetable.getWeight();
+        }
+        return totalWeight;
+    }
+
+    public static void sortIngredientsByCalories(Salad salad) {
+        Collections.sort(salad.getIngredients(), new Comparator<Vegetable>() {
+            @Override
+            public int compare(Vegetable o1, Vegetable o2) {
+                return (int)(o1.getCalories()-o2.getCalories());
+            }
+        });
     }
 }
