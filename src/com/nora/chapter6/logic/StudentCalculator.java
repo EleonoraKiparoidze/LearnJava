cecc5042095769c6bd182a6e469bc0842ae531a7
EleonoraNora <kiparoidzie@gmil.com>
2017-01-26 10:44:33 +0200

    use java.time.temporal.ChronoUnit

--- src/com/nora/chapter6/logic/StudentCalculator.java
+++ src/com/nora/chapter6/logic/StudentCalculator.java
@@ -1,31 +1,29 @@
 package com.nora.chapter6.logic;
 
-import com.nora.chapter6.controller.PartTimeStudent;
 import com.nora.chapter6.controller.Student;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
-import java.util.Calendar;
 import java.util.List;
 
 /**
  * Created by nora on 25.01.17.
  */
-public  class StudentCalculator {
+public class StudentCalculator {
 
-    public static int getAge(Student student){
-        Temporal dayOfBirthday = LocalDate.of(student.getYear(),student.getMonth(),student.getDay());
+    public static int getAge(Student student) {
+        Temporal dayOfBirthday = LocalDate.of(student.getYear(), student.getMonth(), student.getDay());
         Temporal dayToday = LocalDate.now();
-        return (int) ChronoUnit.YEARS.between(dayOfBirthday,dayToday);
+        return (int) ChronoUnit.YEARS.between(dayOfBirthday, dayToday);
     }
 
-    public static int averageMark(Student student){
+    public static int averageMark(Student student) {
         List<Integer> marks = student.getMark();
         int averageMark = 0;
-        for(int mark: marks){
-            averageMark +=mark;
+        for (int mark : marks) {
+            averageMark += mark;
         }
-        return averageMark/marks.size();
+        return averageMark / marks.size();
     }
 }
