d081864e001ccf43467b74d56866e1aed7819637
EleonoraNora <kiparoidzie@gmil.com>
2017-02-04 14:21:02 +0200

    Add exception

--- src/com/nora/chapter8/logic/DataAdder.java
+++ src/com/nora/chapter8/logic/DataAdder.java
@@ -12,42 +12,56 @@ import java.util.Scanner;
 public class DataAdder {
     public static void addInformation() {
 
-        //System.out.println("Enter flower-girl name -->");
         Scanner scanner = new Scanner(System.in);
-        //String flowerGirlName = scanner.next();
-        String flowerGirlName = "Eleonora";
-
-        //////
-        // ///Сделать радобную загрузку цветочницы
-        ////
+        System.out.println("Enter flower-girl name -->");
+        String flowerGirlName = scanner.next();
         FlowerGirl flowerGirl = new FlowerGirl(flowerGirlName);
+
+        System.out.println("Menu -->");
+        System.out.println("1. Add flowers to flower girl;");
+        System.out.println("2. Make bouquet;");
+        System.out.println("3. Sort flowers in bouquet by freshness;");
+        System.out.println("4. Print flowers, that has some Stem Length;");
+        System.out.println("Select operation -->");
+
+        int operation = scanner.nextInt();
+
+        switch (operation) {
+
+            case 1: {
+                try {
+                    addFlowersToFlowerGirl(flowerGirl);
+                } catch (ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+                    System.err.println("We don't have such flower! " + e);
+                } catch (FlowerLogicException e) {
+                    System.err.println(e.getCause());
+                }
+            }
+            break;
+
+        }
+    }
+
+    private static void addFlowersToFlowerGirl(FlowerGirl flowerGirl) throws ClassNotFoundException, IllegalAccessException, InstantiationException, FlowerLogicException {
+
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Add flowers to flower-girl.Enter flower name -->");
         String flowerName = scanner.next();
+        System.out.println("Add flowers to flower-girl.Enter quantity -->");
+        int quantity = scanner.nextInt();
 
-        //System.out.println("Add flowers to flower-girl.Enter quantity -->");
-        //int quantity = (int) scanner.nextDouble();
-        int quantity = 10;
+        Class c = Class.forName("com.nora.chapter8.controller." + flowerName);
+        Object obj = c.newInstance();
+        Flower flower = (Flower) obj;
 
-
-        Flower flower = null;
-        try {
-            Class c = Class.forName("com.nora.chapter8.controller." + flowerName);
-            Object obj = c.newInstance();
-            flower = (Flower) obj;
-
-        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException e) {
-            System.err.println("We don't have such flower! " + e);
-        }
         assert flower != null;
         flower.setName(flowerName);
-        flower.setFreshness(100);
-        flower.setStemLength(10);
-        try {
-            flowerGirl.addFlower(flower, quantity);
-        } catch (FlowerLogicException e) {
-            System.err.println(e.getCause());
-        }
-        System.out.println(flowerGirl);
+        System.out.println("Enter freshness -->");
+        flower.setFreshness(scanner.nextDouble());
+        System.out.println("Enter stem length -->");
+        flower.setStemLength(scanner.nextDouble());
+        flowerGirl.addFlower(flower, quantity);
 
+        System.out.println(flowerGirl);
     }
 }
